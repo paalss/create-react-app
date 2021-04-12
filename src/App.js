6e5a2a4705f63def8539b64b12b2e9245bbd269c
paalss <paalss94@gmail.com>
2021-04-12 16:57:14 +0200

    legger til litt flex layout og react components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from "./logo.svg";
 import "./App.css";
+import ComponentsShowcase from "./componentsShowcase";
+// app bar
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <img src={logo} className="App-logo" alt="logo" />
+            <Typography variant="h6" className={classes.title}>
+              React Material UI components
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <main>
+        <ComponentsShowcase></ComponentsShowcase>
+      </main>
     </div>
   );
 }
---
+++ src/componentsShowcase.js
@@ -0,0 +1,77 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+// showcased components
+import SimplePaper from "./showCasedComponents/Paper"
+import ContainedButtons from "./showCasedComponents/Buttons"
+
+// accordion
+
+
+const useStyles = makeStyles({
+  root: {
+    // minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+function ComponentsShowcase() {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+  return (
+    <div className="ComponentsShowcase">
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            Buttons
+          </Typography>
+          <ContainedButtons></ContainedButtons>
+        </CardContent>
+      </Card>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            Paper
+          </Typography>
+          <SimplePaper></SimplePaper>
+        </CardContent>
+      </Card>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+            Word of the Day
+          </Typography>
+          <Typography variant="h5" component="h2">
+            be{bull}nev{bull}o{bull}lent
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            adjective
+          </Typography>
+          <Typography variant="body2" component="p">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+}
+
+export default ComponentsShowcase;
